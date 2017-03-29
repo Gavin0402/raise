9e219dd84095768b8f91b4bef13174f0b804c59b
wangyingying1217 <498505746@qq.com>
2017-03-29 23:23:23 +0800

    更改表单验证方式

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ var app = new Vue({
   el: "#app",
   data: {
     title: ["热门众筹", "历史众筹", "发起众筹"],//菜单标题
-    tabIndex: {num: 0},//菜单索引
+    tabIndex: 0,//菜单索引
     /*热门数据*/
     hot: [],
     /*历史数据*/
@@ -18,30 +18,21 @@ var app = new Vue({
     build: {
       formDrl: "http://192.168.8.144:8081/raise/fundingSub.jhtml",//表单提交地址
       mediaDrl: "http://192.168.8.144:8081/raise/upload_file.jhtml",//表单提交地址
+      imgAccept: "image/jpeg, image/jpg, image/bmp, image/gif, image/png",
+      videoAccept: "video/avi, video/rmvb, video/3GP, video/wmv, video/mkv, video/mp4, video/wmv, video/mov",
       /*数据*/
       identity: ["个人", "机构"],//身份类型数据
       type: [],//众筹类型数据
       /*索引*/
-      pageIndex: {num: 0},//页面索引
-      identityIndex: {num: 0},//身份类型索引
-      typeIndex: {num: 0},//众筹类型索引
-      IDinfo: {
-        IDtype: ["身份证", "护照"],//证件类型
-        IDnum: "",//证件类型
-        IDtypeSelect: "身份证",//选中证件类型
-        IDerrors: "请填写有效的18位身份证号码",//选中证件类型
-      },
-      /*图片路径*/
-      IDcorrectSrc: {src: ""},//证件正面照
-      IDoppositeSrc: {src: ""},//证件反面照
-      businessLicenseSrc: {src: ""},//营业执照照片
-      coverPicSrc: {src: ""},//封面照片
-      advertiseVideoSrc: {src: ""},//宣传视频
+      pageIndex: 0,//页面索引
+      identityIndex: 0,//身份类型索引
+      typeIndex: 0,//众筹类型索引
+      advertiseVideoSrc: "",//宣传视频
       /*项目详情*/
       detailsDescription: [
         {
           type: 0,//添加类型（文本/图片）
-          edit: {num: 0},
+          edit: 0,
           title: "为什么我需要你的资金支持",
           content: "<p>请在这里说明你的项目特色，以及详细的资 金用途，这会增加项目的可信度并由此提高筹资的成功率。</p>",
           contentEdit: "请在这里说明你的项目特色，以及详细的资 金用途，这会增加项目的可信度并由此提高筹资的成功率。",
@@ -49,7 +40,7 @@ var app = new Vue({
         },
         {
           type: 1,//添加类型（文本/图片）
-          edit: {num: 0},
+          edit: 0,
           title: "",
           content: "",
           contentEdit: "",
@@ -68,7 +59,7 @@ var app = new Vue({
       /*添加图片*/
       newPic: {
         type: 1,//添加类型（文本/图片）
-        edit: {num: 0},
+        edit: 0,
         title: "",
         content: "",
         contentEdit: "",
@@ -82,20 +73,20 @@ var app = new Vue({
           ],
           type: 1,
           lotteryNum: "抽出1个奖品",
-          freight: {"invalid": ""},
-          lotteryReg: {"invalid": ""},
+          freight: {invalid: "", value: ""},
+          lotteryReg: {invalid: "", value: ""},
           normal: {
-            faMoney: {"invalid": ""},
-            reTitlw: {"invalid": ""},
-            content: {"invalid": ""},
-            faPeople: {"invalid": ""},
-            retime: {"invalid": ""},
+            faMoney: {invalid: "", value: ""},
+            reTitlw: {invalid: "", value: ""},
+            content: {invalid: "", value: ""},
+            faPeople: {invalid: "", value: ""},
+            retime: {invalid: "", value: ""},
           },
           lottery: {
-            faMoney: {"invalid": ""},
-            content: {"invalid": ""},
-            faPeople: {"invalid": ""},
-            retime: {"invalid": ""},
+            faMoney: {invalid: "", value: ""},
+            content: {invalid: "", value: ""},
+            faPeople: {invalid: "", value: ""},
+            retime: {invalid: "", value: ""},
           },
         }
       ],
@@ -106,59 +97,75 @@ var app = new Vue({
         ],
         type: 1,
         lotteryNum: "抽出1个奖品",
-        freight: {"invalid": ""},
-        lotteryReg: {"invalid": ""},
+        freight: {invalid: "", value: ""},
+        lotteryReg: {invalid: "", value: ""},
         normal: {
-          faMoney: {"invalid": ""},
-          reTitlw: {"invalid": ""},
-          content: {"invalid": ""},
-          faPeople: {"invalid": ""},
-          retime: {"invalid": ""},
+          faMoney: {invalid: "", value: ""},
+          reTitlw: {invalid: "", value: ""},
+          content: {invalid: "", value: ""},
+          faPeople: {invalid: "", value: ""},
+          retime: {invalid: "", value: ""},
         },
         lottery: {
-          faMoney: {"invalid": ""},
-          content: {"invalid": ""},
-          faPeople: {"invalid": ""},
-          retime: {"invalid": ""},
+          faMoney: {invalid: "", value: ""},
+          content: {invalid: "", value: ""},
+          faPeople: {invalid: "", value: ""},
+          retime: {invalid: "", value: ""},
         },
       },
     },
     position: [],
-    city1: "",
-    town1: {},
-    city2: "",
-    town2: {},
+    city1: {},
+    city2: {},
     page1: {
       per: {
-        username: {"invalid": ""},
-        IDnum: {"invalid": ""},
-        telnum: {"invalid": ""},
-        loProvince: {"invalid": ""},
-        positive: {"invalid": ""},
-        inverse: {"invalid": ""},
+        username: {invalid: "", value: ""},
+        IDnum: {invalid: "", value: ""},
+        telnum: {invalid: "", value: ""},
+        loProvince: {invalid: "", value: ""},
+        positive: {invalid: "", value: ""},
+        inverse: {invalid: "", value: ""},
       },
       ins: {
-        tradeName: {"invalid": ""},
-        license: {"invalid": ""},
-        lawPerson: {"invalid": ""},
-        inRegister: {"invalid": ""},
-        contact: {"invalid": ""},
-        qyPhone: {"invalid": ""},
-        licenseImg: {"invalid": ""},
+        tradeName: {invalid: "", value: ""},
+        license: {invalid: "", value: ""},
+        lawPerson: {invalid: "", value: ""},
+        inRegister: {invalid: "", value: ""},
+        contact: {invalid: "", value: ""},
+        qyPhone: {invalid: "", value: ""},
+        licenseImg: {invalid: "", value: ""},
       },
     },
     page2: {
-      cover: {"invalid": ""},
-      cfName: {"invalid": ""},
-      purpose: {"invalid": ""},
-      totaltarget: {"invalid": ""},
-      timelimit: {"invalid": ""},
-      chuziShare: {"invalid": ""},
-      itemProvince: {"invalid": ""},
+      cover: {invalid: "", value: ""},
+      cfName: {invalid: "", value: ""},
+      purpose: {invalid: "", value: ""},
+      totaltarget: {invalid: "", value: ""},
+      timelimit: {invalid: "", value: ""},
+      chuziShare: {invalid: "", value: ""},
+      itemProvince: {invalid: "", value: ""},
     },
     selfdomValid: {
+      value: "",
       selfdomlength: {"invalid": false},
       selfdomnum: {"invalid": false},
+    },
+    validateMsg: {
+      unempty: "*此项不能为空",
+      usename: "*请输入正确的中文名字(2~5个汉字之间)",
+      IDnum: "*请填写有效的18位身份证号码",
+      telnum: "*请填写正确有效的11位手机号",
+      location: "*请选择地点",
+      pic: "*请上传图片",
+      totaltarget: "*筹资金额不低于500 , 最高1亿'",
+      timelimit: "*筹资时间周期须在1～31天之内",
+      chuziShare: "*出资份额须在1~100之间'",
+      selfdomlength: "*每个标签不超过5个字符",
+      selfdomnum: "*最多输入三个自定义标签",
+      faMoney: "",
+      faPeople: "",
+      freight: "",
+      retime: "",
     }
   },
   validators: { // `numeric` and `url` custom validator is local registration
@@ -203,11 +210,19 @@ var app = new Vue({
     },
   },
   methods: {
-    switchIndex: function (index, obj) {
-      obj.num = index;
-    },
-    deleteSrc: function (obj) {
-      obj.src = "";
+    getCustomers: function () {
+    this.$http.get(apiURL).then(function (response) {
+      console.log(response.data);
+      this.hot = response.data.hot;
+      this.history = response.data.his;
+      this.info = response.data.info;
+      this.build.type = response.data.itemtype;
+    })
+  },
+    pisitionData: function () {
+      this.$http.get("../js/city.json").then(function (response) {
+        app.position = response.body.citylist;
+      })
     },
     saveEdit: function (item) {
       var arr = item.contentEdit.split("\n");
@@ -216,10 +231,7 @@ var app = new Vue({
         str += "<p>" + arr[i] + "</p>";
       }
       item.content = str;
-      item.edit.num = 0;
-    },
-    delArr: function (obj, index) {
-      obj.splice(index, 1);
+      item.edit = 0;
     },
     addDescription: function (parent, children, num) {
       if (parent.length > num - 1) {
@@ -232,74 +244,54 @@ var app = new Vue({
       if (item.picSrc.length > 3) {
         alert("最多只能上传" + 3 + "条");
       } else {
-        this.fileSubmit(event, id, obj);
+        this.fileSubmit( id, obj);
         item.picSrc.push({src: ""});
       }
     },
-    getCustomers: function () {
-      this.$http.get(apiURL).then(function (response) {
-        console.log(response.data);
-        this.hot = response.data.hot;
-        this.history = response.data.his;
-        this.info = response.data.info;
-        this.build.type = response.data.itemtype;
-      })
-    },
-    fileSubmit: function (e, id, obj, valid) {
+    fileSubmit: function (id, valid, method) {
       var This = this;
       var formData = new FormData(document.getElementById(id));//表单id
-      this.$http.post(this.build.mediaDrl, formData).then(function (data) {
-        obj.src = data.data.filePath;
-        valid && This.handleValidation(e, valid);
+      this.$http.post(this.build.mediaDrl, formData).then(function (res) {
+        this[valid].value = res.data.filePath;
+        method && This.validate(method, valid);
       }, function (response) {
         alert("上传失败");
       });
     },
-    IDtypeMethod: function (event) {
-      if (this.build.IDinfo.IDtypeSelect == "身份证") {
-        this.$options.validators.IDnum = function (val) {
-          return /^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$|^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}([0-9]|X|x)$/.test(val);
-        };
-        this.build.IDinfo.IDerrors = "请填写有效的18位身份证号码";
-      } else if (this.build.IDinfo.IDtypeSelect == "护照") {
-        this.$options.validators.IDnum = function (val) {
-          return /^1[45][0-9]{7}|G[0-9]{8}|P[0-9]{7}|S[0-9]{7,8}|D[0-9]+$/.test(val);
-        };
-        this.build.IDinfo.IDerrors = "请填写有效的18位";
+    townData: function (city, method, valid) {
+      for (var i = 0; i < this.position.length; i++) {
+        if (this.position[i].p == valid.value) {
+          this[city] = JSON.parse(JSON.stringify(this.position[i].c));
+        }
       }
-      this.page1.per.IDnum.invalid = !this.$options.validators.IDnum(this.build.IDinfo.IDnum);
+      this.validate(method, valid);
+    },
+    validate: function (reg, item) {
+      item.invalid = !this.$options.validators[reg](item.value);
+    },
+    selfdomValidate: function () {
+      this.selfdomValid.selfdomlength.invalid = !this.$options.validators.selfdomlength(this.selfdomValid.value);
+      this.selfdomValid.selfdomnum.invalid = !this.$options.validators.selfdomnum(this.selfdomValid.value);
     },
     submitPage1: function (e) {
       var bOff = true;
-      if (this.build.identityIndex.num == 0) {
-        var page = this.page1.per;
+      if (this.build.identityIndex == 0) {
+        bOff = _check(this.page1.per);
       } else {
-        var page = this.page1.ins;
-      }
-      for (attr in page) {
-        if (page[attr].invalid === "" || page[attr].invalid === true) {
-          page[attr].invalid = true;
-          bOff = false;
-        }
+        bOff = _check(this.page1.ins);
       }
       if (bOff) {
-        this.switchIndex(1, app.build.pageIndex);
+        this.build.pageIndex = 1;
       }
     },
     submitPage2: function (e) {
       var bOff = true;
-      var page = this.page2;
-      for (attr in page) {
-        if (page[attr].invalid === "" || page[attr].invalid === true) {
-          page[attr].invalid = true;
-          bOff = false;
-        }
-      }
+      bOff = _check(this.page2);
       if (this.selfdomValid.selfdomlength.invalid || this.selfdomValid.selfdomnum.invalid) {
         bOff = false;
       }
       if (bOff) {
-        this.switchIndex(2, app.build.pageIndex);
+        this.build.pageIndex = 2;
       }
     },
     submitPage3: function (e) {
@@ -309,43 +301,17 @@ var app = new Vue({
         details[i].edit.num && (bOff = false);
       }
       if (bOff) {
-        this.switchIndex(3, app.build.pageIndex);
+        this.build.pageIndex = 3;
       } else {
         alert("您还有为完成的编辑项");
       }
-    },
-    pisitionData: function () {
-      this.$http.get("../js/city.json").then(function (response) {
-        app.position = response.body.citylist;
-      })
-    },
-    townData: function (town, city, valid) {
-      for (var i = 0; i < this.position.length; i++) {
-        if (this.position[i].p == city) {
-          town.town = JSON.parse(JSON.stringify(this.position[i].c));
-        }
-      }
-      this.handleValidation(event, valid);
-    },
-    handleValidation: function (e, item) {
-      var $validity = e.target.$validity
-      $validity.validate(function () {
-        item.invalid = $validity.result.invalid;
-      });
-    },
-    selfdomValidation: function (e) {
-      var $validity = e.target.$validity
-      $validity.validate(function () {
-        app.selfdomValid.selfdomlength.invalid = $validity.result.selfdomlength;
-        app.selfdomValid.selfdomnum.invalid = $validity.result.selfdomnum;
-      });
     },
     submit: function () {
       var returnInfo = this.build.returnInfo;
       var bOff = true;
       for (var i = 0; i < returnInfo.length; i++) {
         if (returnInfo[i].type == 3) {
-          var removeNode = document.getElementById("normal"+i);
+          var removeNode = document.getElementById("normal" + i);
           removeNode.parentNode.removeChild(removeNode);
           for (attr in returnInfo[i].lottery) {
             if (returnInfo[i].lottery[attr].invalid === "" || returnInfo[i].lottery[attr].invalid === true) {
@@ -360,7 +326,7 @@ var app = new Vue({
             }
           }
         } else {
-          var removeNode = document.getElementById("lottery"+i);
+          var removeNode = document.getElementById("lottery" + i);
           removeNode.parentNode.removeChild(removeNode);
           for (attr in returnInfo[i].normal) {
             if (returnInfo[i].normal[attr].invalid === "" || returnInfo[i].normal[attr].invalid === true) {
@@ -408,3 +374,13 @@ var app = new Vue({
     this.pisitionData();
   }
 });
+
+function _check(page) {
+  for (attr in page) {
+    if (page[attr].invalid === "" || page[attr].invalid === true) {
+      page[attr].invalid = true;
+      bOff = false;
+    }
+  }
+  return bOff;
+}
